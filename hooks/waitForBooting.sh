01e3fb3e6f1f076da2b9870db711cd2f5007eec4
neil <github@neilpang.com>
2022-08-03 18:22:12 +0800

    minor, speedup

--- hooks/waitForBooting.sh
+++ hooks/waitForBooting.sh
@@ -2,14 +2,14 @@
 
 
 echo "====> Wait for Default: F"
-if bash vbox.sh waitForText $VM_OS_NAME "Default: F" 2 ; then
+if bash vbox.sh waitForText $VM_OS_NAME "Default: F" 5 ; then
   echo "====> OK, enter"
   bash vbox.sh enter
 fi
 
 
 echo "====> Wait for Booting in"
-if bash vbox.sh waitForText $VM_OS_NAME "Booting in" 3 ; then
+if bash vbox.sh waitForText $VM_OS_NAME "Booting in" 7 ; then
   echo "====> OK, enter"
   bash vbox.sh enter
 fi
